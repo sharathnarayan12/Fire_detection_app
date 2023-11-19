b05bafbc26f22f67c9edb1224dd9e91a03e0b1bf
sharathnarayan12 <sharathrammadanapuri12@gmail.com>
2023-11-19 19:44:07 +0530

    2nd commit

--- fire_detection.sh
+++ fire_detection.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Specify the Python version
-PYTHON_VERSION="3.8"
+echo "Create a virtual environment using virtualenv"
+
+# Check if python3-virtualenv is installed
+if ! command -v virtualenv &> /dev/null; then
+    # Install python3-virtualenv
+    sudo apt install python3-virtualenv
+fi
 
 # Check if the virtual environment exists
 if [ ! -d "env" ]; then
-    # Create a virtual environment
-    python${PYTHON_VERSION} -m venv env
+    # Create a virtual environment using virtualenv
+    virtualenv env
     # Activate the virtual environment
     source env/bin/activate
-    # Install requirements using pip
-    pip install -r requirements.txt
+
 else
     # Activate the existing virtual environment
     source env/bin/activate
 fi
 
-# Create a directory called fire_detection_app if it doesn't exist
-mkdir -p fire_detection_app
+echo "cloneing repo..... plz wait"
 
-# Change to the fire_detection_app directory
-cd fire_detection_app
+# Clone the Yolov_5_fire_detection repository if it doesn't exist
+if [ ! -d "Yolov_5_fire_detection" ]; then
+    git clone https://github.com/sharathnarayan12/Yolov_5_fire_detection.git
+    cd Yolov_5_fire_detection
+fi
+
+echo "cloning yolov5"
 
 # Check if the yolov5 repository exists
 if [ ! -d "yolov5" ]; then
@@ -30,28 +38,15 @@ if [ ! -d "yolov5" ]; then
     cd yolov5
     # Install requirements using pip
     pip install -r requirements.txt
-    # Move back to the parent directory
-    cd ..
+    echo "Environment created"
 fi
 
-# Check if the Yolov_5_fire_detection repository exists
-if [ ! -d "Yolov_5_fire_detection" ]; then
-    # Clone another repository (Yolov_5_fire_detection)
-    git clone https://github.com/sharathnarayan12/Yolov_5_fire_detection.git
-fi
-
-# Move into the Yolov_5_fire_detection directory
-cd yolov5
-
-# Check if a source input is provided
+# Run the Python script (detect.py) with the provided source input
 if [ -z "$1" ]; then
     echo "Please provide a source input."
 else
-    # Run the Python script (detect.py) with the provided source input
     python detect.py --source "$1" --weights ../models/best.pt --conf 0.2
 fi
 
-# Deactivate the virtual environment
-deactivate
-
-echo "Environment setup and script execution completed successfully."
+# No deactivation until manually interrupted (e.g., Ctrl + Z)
+echo "Script completed successfully."
