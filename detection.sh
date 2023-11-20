ad7eb2d9fc2c9b152b2309873ff3155d9dbab2f1
I AM Batman ++ <93177242+sharathnarayan12@users.noreply.github.com>
2023-11-20 09:32:03 +0530

    Update detection.sh
    typo

--- detection.sh
+++ detection.sh
@@ -3,7 +3,7 @@
 # Activate the virtual environment
 source env/bin/activate
 
-cd /home/sharath/Batman/yolov5_fire_detection/Fire_detection_app/Yolov_5_fire_detection/yolov5
+cd ../yolov5_fire_detection/Fire_detection_app/Yolov_5_fire_detection/yolov5
 
 # Run the Python script (detect.py) with the provided source input
 if [ -z "$1" ]; then
